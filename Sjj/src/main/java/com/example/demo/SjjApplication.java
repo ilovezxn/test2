97500f34f2a7e7bc390069b3864060217cb31e58
1181748958@qq.com <1181748958@qq.com>
2021-11-24 04:24:02 +0800

    first

---
+++ Sjj/src/main/java/com/example/demo/SjjApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjjApplication.class, args);
+	}
+
+}
